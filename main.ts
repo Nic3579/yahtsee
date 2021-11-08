f5b26524f0c65078bb8155f9e8b77827e0ca2032
Nic3579 <92817235+Nic3579@users.noreply.github.com>
2021-11-08 17:44:32 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -78,6 +78,36 @@ input.onGesture(Gesture.Shake, function () {
         dice_5 = randint(1, 6)
     }
 })
+// this block should do nothing to the program. it is meant to hold the ideas of what the game needs to have
+// - 5 different dice
+// - a player wins when they get all 5 of the dice to show the same number
+// - each player's turn consists of up to 3 die rolls
+// - a player wins when they get all of the dice to show the same number
+// - players may choose what dice they roll, and what ones they don't
+// - a display to show what the dice values are
+// - 
+basic.forever(function () {
+    if (false) {
+        basic.showString("goals")
+    }
+})
+// this block should do nothing to the program. it is meant to hold the instructions on how to play.
+// each turn a player will:
+// - roll the dice 3 times
+// - save dice (not roll them)
+// - attempt to get all the dice to show the same number
+// 
+// A player wins when they make all of the  dice show the same number
+// 
+// - To roll, a player must shake the micro:bit.
+// - to toggle weather or not a dice will be rolled, the player must press the corresponding button/input
+// - to end their turn, a player should have all of the dice saved or run out of rolls
+// - 
+basic.forever(function () {
+    if (false) {
+        basic.showString("instructions")
+    }
+})
 // repeatedly displays the dice values forever
 basic.forever(function () {
     // displays the value of dice 1
@@ -116,5 +146,10 @@ basic.forever(function () {
         s4 = 0
         s5 = 0
         shake_counter = 0
+    } else {
+        // ends turn
+        if (shake_counter >= 3 || s1 + s2 + (s3 + s4 + s5) == 5) {
+            basic.showString("Turn over")
+        }
     }
 })
